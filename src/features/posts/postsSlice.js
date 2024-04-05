8008a3ed79b7fc2ce6b304551d4c2990f5cb7078
led-dotcom <74643143+led-dotcom@users.noreply.github.com>
2024-04-04 22:21:05 -0400

    feat: Fix variable name in postsSlice.js

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -29,7 +29,7 @@ const postsSlice = createSlice({
   reducers: {
     postUpdated(state, action) {
       const { id, title, content } = action.payload
-      const existingPost = state.posts.find((post) => post.id === postId)
+      const existingPost = state.posts.find((post) => post.id === id)
       if (existingPost) {
         existingPost.title = title
         existingPost.content = content
@@ -37,7 +37,7 @@ const postsSlice = createSlice({
     },
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload
-      const existingPost = state.posts.find((post) => post.id === id)
+      const existingPost = state.posts.find((post) => post.id === postId)
       if (existingPost) {
         existingPost.reactions[reaction]++
       }
